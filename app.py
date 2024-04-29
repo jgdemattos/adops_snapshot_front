4e13c98d98db792104be737d96f057376f62b339
agencialaranjeira <joaomattos@agencialaranjeira.com.br>
2024-04-29 17:29:56 -0300

    first chart

--- app.py
+++ app.py
@@ -1,24 +1,53 @@
 import streamlit as st
 import pandas as pd
+import os
+from dotenv import load_dotenv, dotenv_values #pip
+from supabase import create_client, Client
+import plotly.express as px
 
-st.set_page_config(page_title="Meu Site Streamlit")
+load_dotenv()
 
-with st.container():
-    st.subheader("Meu primeiro site com o Streamlit")
-    st.title("Dashboard de Contratos")
-    st.write("Informações sobre os contratos fechados pela Hash&Co ao longo de maio")
-    st.write("Quer aprender Python? [Clique aqui](https://www.hashtagtreinamentos.com/curso-python)")
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 
-@st.cache_data
-def carregar_dados():
-    tabela = pd.read_csv("resultados.csv")
-    return tabela
+st.set_page_config(page_title="Meu Site Streamlit",layout="wide")
 
-with st.container():
-    st.write("---")
-    qtde_dias = st.selectbox("Selecione o período", ["7D", "15D", "21D", "30D"])
-    num_dias = int(qtde_dias.replace("D", ""))
-    dados = carregar_dados()
-    dados = dados[-num_dias:]
-    st.area_chart(dados, x="Data", y="Contratos")
+response = supabase.table('client_daily').select("date,cod,health,satisfaction,engagement,budget,spend,projection").lt('date', '2024-04-30').gt('date', '2024-04-01').execute()
+
+df = pd.DataFrame(response.data)
+df["date"] = pd.to_datetime(df["date"])
+df=df.sort_values("date")
+
+cod=st.sidebar.selectbox("Client", list(set(df["cod"])))
+
+df_filtered = df[df["cod"] == cod]
+
+df_filtered
+
+count_data = df_filtered['health'].value_counts().reset_index()
+count_data.columns = ['health', 'count']
+
+st.write("DataFrame showing the count of each category:")
+st.write(count_data)
+
+# Plotting using Plotly Express
+fig = px.bar(count_data, x='health', y='count',
+             labels={'Counts': 'Total Counts'},
+             title='Bar Chart of Category Counts')
+fig.update_layout(xaxis_title='Category', yaxis_title='Total Counts')
+
+# Display the plot in Streamlit
+st.plotly_chart(fig)
+
+#col1,col2=st.columns(2)
+
+#fig_date=px.bar(df_filtered, x="health",y="date", title="Saúde em dias")
+#col1.plotly_chart(fig_date)
+
+#print(response)
+#print(response.data)
+
+
+
